c4e5111a441bf8d5d61c5d685adbab5a5d43e46b
Ajdin <ajdin.aliu@gmail.com>
2015-07-03 00:40:57 +0200

    initial commit
    chocolatey and chocolatey.powershell templates

---
+++ chocolatey.powershell/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿#$tools = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+#. (Join-Path $tools PSCollectServerInfo.ps1) $env:computername
+
+$psFile = Join-Path "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" '__NAME__.ps1'
+#$psArgs = $env:computername
+
+Start-ChocolateyProcessAsAdmin "& `'$psFile`'"
